16bb1414250929ba25543e4be43a5d39bb0708cf
x1ang <x1angji@qq.com>
2025-10-02 23:47:09 +0800

    Site updated: 2025-10-02 23:47:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/28/笔记本氮化镓电源使用指北/","2024/10/18/紫金山-阿特拉斯彗星/","2025/03/15/C-查漏补缺/","2025/09/19/butterfly主题魔改/"];function toRandomPost(){
+var posts=["2024/10/18/紫金山-阿特拉斯彗星/","2024/10/28/笔记本氮化镓电源使用指北/","2025/03/15/C-查漏补缺/","2025/09/19/butterfly主题魔改/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
